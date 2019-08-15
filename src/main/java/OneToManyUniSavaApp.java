d062d82a10ce5c1d1b5bf15fa10753299e0f6031
LukaszP <lukaszp4@onet.eu>
2019-08-15 17:27:17 +0200

    Initial commit

---
+++ src/main/java/OneToManyUniSavaApp.java
@@ -0,0 +1,49 @@
+import Entity.Company;
+import Entity.CompanyDetail;
+import Entity.Department;
+import Entity.Property;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class OneToManyUniSavaApp {
+
+    public static void main(String[] args) {
+        // stworzyc obiekt Configuration
+        Configuration conf = new Configuration().configure("hibernate.cfg.xml");
+        // wczytanie adnotacji
+        conf.addAnnotatedClass(Company.class);
+        conf.addAnnotatedClass(CompanyDetail.class);
+        conf.addAnnotatedClass(Property.class);
+        conf.addAnnotatedClass(Department.class);
+
+        // stworzenie obiektu SessionFactory
+        SessionFactory factory = conf.buildSessionFactory();
+
+        // pobranie sesji
+        Session session = factory.getCurrentSession();
+
+        int id = 41;
+
+        session.beginTransaction();
+        Company company = session.get(Company.class, id);
+        System.out.println("Informacje dotyczące firmy" + company);
+
+        Department department1 = new Department("sales");
+        Department department2 = new Department("production");
+        Department department3 = new Department("HR");
+
+        company.addDepartment(department1);
+        company.addDepartment(department2);
+        company.addDepartment(department3);
+
+        session.persist(company); //operacje dodawania jest kaskadowalna
+
+        session.getTransaction().commit();
+
+        factory.close();
+    }
+
+}
---
+++ src/main/java/OneToManyUniSaveApp.java
@@ -0,0 +1,49 @@
+import Entity.Company;
+import Entity.CompanyDetail;
+import Entity.Department;
+import Entity.Property;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Set;
+
+public class OneToManyUniSaveApp {
+
+    public static void main(String[] args) {
+        // stworzyc obiekt Configuration
+        Configuration conf = new Configuration().configure("hibernate.cfg.xml");
+        // wczytanie adnotacji
+        conf.addAnnotatedClass(Company.class);
+        conf.addAnnotatedClass(CompanyDetail.class);
+        conf.addAnnotatedClass(Property.class);
+        conf.addAnnotatedClass(Department.class);
+
+        // stworzenie obiektu SessionFactory
+        SessionFactory factory = conf.buildSessionFactory();
+
+        // pobranie sesji
+        Session session = factory.getCurrentSession();
+
+        int id = 41;
+
+        session.beginTransaction();
+   /*     Company company = session.get(Company.class, id);
+        System.out.println("Informacje dotyczące firmy" + company);
+
+        Set<Department> departments = company.getDepartments();
+
+        for(Department department : departments){
+            System.out.println(department);
+        } */
+
+        Department department = session.get(Department.class,3);
+        System.out.println("Info o department" + department);
+        
+
+        session.getTransaction().commit();
+
+        factory.close();
+    }
+
+}
